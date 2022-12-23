71f62d82a9921ed7d32f00dd82d598350592fe6d
Emil <mawlitov.e@yandex.ru>
2022-12-23 04:19:33 +0400

    123

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+
+// Console.WriteLine("Введите число, в котором хотите увидеть вторую цифру");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+//  int a = num / 10 % 10; 
+ 
+// {
+//     Console.WriteLine($"{a}");
+// }
+//   ГОТОВО!!!
+
+
+ //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+// Console.Write("Введи число: ");
+// int anyNumber = Convert.ToInt32(Console.ReadLine());
+// string anyNumberText = Convert.ToString(anyNumber);
+// if (anyNumberText.Length > 2){
+//   Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+// }
+// else {
+//   Console.WriteLine("-> третьей цифры нет");
+// }
+//    ГОТОВО!!!
